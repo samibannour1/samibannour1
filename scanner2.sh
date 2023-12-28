df60390f6e5d695b3c7888f68fe01655d3cdc836
samibannour1 <155062757+samibannour1@users.noreply.github.com>
2023-12-28 10:29:42 +0100

    Add files via upload

---
+++ scanner2.sh
@@ -0,0 +1,11 @@
+echo "  ______ ____   _____ _    _  _____ "
+echo " |  ____/ __ \ / ____| |  | |/ ____|"
+echo " | |__ | |  | | |    | |  | | (___  "
+echo " |  __|| |  | | |    | |  | |\___ \ "
+echo " | |   | |__| | |____| |__| |____) |"
+echo " |_|    \____/ \_____|\____/|_____/ "
+echo "                                    "
+echo "                                    "
+echo Scanner puzzle 130  Started
+echo Donations : bc1q962duatv26hzw080uxu65damn06l5pgsdnx6lt
+./focushunt -m bsgs -f p130.txt  -r  0000000000000000000000000000000200000000000000000000000000000000:00000000000000000000000000000003ffffffffffffffffffffffffffffffff   -B random  -k 300  -t 8 -S  -M
